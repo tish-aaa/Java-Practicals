274337e25cf172589495d5938ed5fd98d9b670eb
Tisha Sharma <124681335+tish-aaa@users.noreply.github.com>
2023-08-07 13:42:52 +0530

    single_inheritance demo

---
+++ Inheritance/single_inheritance.java
@@ -0,0 +1,29 @@
+class add
+{
+	double i,j;
+	double show(double a, double b)
+	{
+		i=a;
+		j=b;
+		System.out.println("1st Number: "+i+" and 2nd Number: "+j); 
+		return 0;
+	}
+}
+class total extends add
+{
+	double t;
+	void disp()
+	{
+		t=i+j;
+		System.out.println("Sum of the 2 numbers: "+t);
+	}
+}
+class single_inheritance
+{
+	public static void main(String args[])
+	{
+		total obj=new total();
+		obj.show(1.2,4.5);
+		obj.disp();
+	}
+}
